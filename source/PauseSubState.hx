edba25a050be1e293b99767897daf768ca41b873
idkwhoiam381 <ismotass86@gmail.com>
2025-08-25 11:51:19 +0300

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -11,7 +11,7 @@ import flixel.FlxSubState;
 import flixel.addons.transition.FlxTransitionableState;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.input.keyboard.FlxKey;
-import flixel.system.FlxSound;
+import flixel.sound.FlxSound;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
@@ -276,4 +276,5 @@ class PauseSubState extends MusicBeatSubstate
 			}
 		}
 	}
+
 }
