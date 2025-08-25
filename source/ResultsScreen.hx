b5af44f6437d684c9fd3b9c1c357233118b08cc4
idkwhoiam381 <ismotass86@gmail.com>
2025-08-25 11:49:31 +0300

    Update ResultsScreen.hx

--- source/ResultsScreen.hx
+++ source/ResultsScreen.hx
@@ -2,7 +2,7 @@ package;
 
 import openfl.geom.Matrix;
 import openfl.display.BitmapData;
-import flixel.system.FlxSound;
+import flixel.sound.FlxSound;
 import flixel.util.FlxAxes;
 import Options.Option;
 import flixel.input.FlxInput;
