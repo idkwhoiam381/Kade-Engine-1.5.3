07be7125414697d11cd9eb2929d1b753990c79af
idkwhoiam381 <ismotass86@gmail.com>
2025-08-25 11:47:37 +0300

    Update ChartingState.hx

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -22,7 +22,7 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.group.FlxGroup;
 import flixel.math.FlxMath;
 import flixel.math.FlxPoint;
-import flixel.system.FlxSound;
+import flixel.sound.FlxSound;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.ui.FlxSpriteButton;
@@ -1531,3 +1531,4 @@ class ChartingState extends MusicBeatState
 		FlxG.log.error("Problem saving Level data");
 	}
 }
+
