54b9c98af889ea056ff1cb2a4f535b31e9351204
idkwhoiam381 <ismotass86@gmail.com>
2025-08-25 15:15:29 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -80,7 +80,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 		
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		addChild(game);
 		
